feba4d5bdd115e1043d0dd86450751df9e8d6b32
Martin Androvich <martinandrovich@gmail.com>
2019-03-04 17:23:18 +0100

    Updated main

--- main/pan_tilt.runs/synth_1/main.tcl
+++ main/pan_tilt.runs/synth_1/main.tcl
@@ -28,7 +28,7 @@ set_property default_lib xil_defaultlib [current_project]
 set_property target_language VHDL [current_project]
 set_property ip_output_repo {c:/Users/Martin Androvich/Desktop/rb-pro4/fpga/main/pan_tilt.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_vhdl -library xil_defaultlib {{C:/Users/Martin Androvich/Desktop/rb-pro4/fpga/assets/components/main.vhd}}
+read_vhdl -library xil_defaultlib {{C:/Users/Martin Androvich/Desktop/rb-pro4/fpga/main/pan_tilt.srcs/sources_1/main.vhd}}
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
