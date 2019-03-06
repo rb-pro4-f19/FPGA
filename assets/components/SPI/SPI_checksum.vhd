3a818ab019dfb058f940edd831da214be1f2cb58
Martin Nguyen <mangu17@student.sdu.dk>
2019-03-06 10:42:13 +0100

    Checksum setup
    First checksum setup with variables

--- assets/components/SPI/SPI_checksum.vhd
+++ assets/components/SPI/SPI_checksum.vhd
@@ -35,14 +35,24 @@ port(
 
     data    :   in  std_logic_vector(data_l-1 downto 0);
 
-    ready   :   in  std_logic;
+    ready   :   in  std_logic := true;
 
-    busy    :   out std_logic;
+    busy    :   out std_logic := false;
 
     correct :   out std_logic
 
 );
 end SPI_checksum;
+
+    constant mask_s : integer := 3;
+    constant bitmask: STD_LOGIC_Vector(mask_s downto 0) := "1111";
+
+--    signal b_data_a : std_logic_vector (mask_s downto 0) := "0000";
+--    signal b_data_b : std_logic_vector(mask_s downto 0) := "0000";
+--    signal b_data_c : std_logic_vector(mask_s downto 0) := "0000";
+--    signal b_data_checksum : std_logic_vector(mask_s downto 0) := "0000";
+--    signal state : integer range 0 to 2 := 0;
+
 
 architecture Behavioral of SPI_checksum is
 
@@ -50,13 +60,48 @@ architecture Behavioral of SPI_checksum is
 
     process(clk)
 
-    variable b_index : integer range 0 to data_l-1;
+    --variable b_index : integer range 0 to data_l-1;
+    variable b_data_a : std_logic_vector (mask_s downto 0) := "0000";
+    variable b_data_b : std_logic_vector(mask_s downto 0) := "0000";
+    variable b_data_c : std_logic_vector(mask_s downto 0) := "0000";
+    variable b_data_checksum : std_logic_vector(mask_s downto 0) := "0000";
+    variable state : integer range 0 to 2 := 0;
 
     begin
+      if(ready = true and busy = false) then
 
-    
+        ready <= false;
 
+        busy  <= true;
 
-    end process
+      end if;
+
+      if(ready = false and busy = true) then
+
+        b_data_a        <= data_l(15 downto 12);
+        b_data_b        <= data_l(11 downto 8);
+        b_data_c        <= data_l(7 downto 4);
+
+        b_data_checksum <= shift_left(unsigned(data_l(3 downto 0)), 4);
+
+        b_data_checksum <= shift_left(unsigned((b_data_a + b_data_checksum) and bitmask), 4);
+
+        b_data_checksum <= shift_left(unsigned((b_data_b + b_data_checksum) and bitmask), 4);
+
+        b_data_checksum <= shift_left(unsigned((b_data_c + b_data_checksum) and bitmask), 4);
+
+        if(b_data_checksum = data_l(3 downto 0)) then
+
+          correct <= true;
+
+        else false;
+        end if;
+
+        busy  = false;
+        ready = true;
+
+      end if;
+
+    end process;
 
 end Behavioral;
