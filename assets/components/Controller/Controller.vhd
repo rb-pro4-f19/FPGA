f696558bdcfadd22f031f242b33efa632ed0a6c4
Daniel Tofte S <danieltoftesch@hotmail.com>
2019-03-13 22:48:27 +0100

    New Controller

--- assets/components/Controller/Controller.vhd
+++ assets/components/Controller/Controller.vhd
@@ -24,99 +24,162 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity CONTROLLER is
     port(
-    clk                     :   in  std_logic;
-    sck                     :   in  std_logic;
-    ss                      :   in  std_logic;
-    mosi                    :   in  std_logic;
-    miso                    :   out std_logic
+
+        clk                         :   in  std_logic;
+
+        sck                         :   in  std_logic;
+
+        ss                          :   in  std_logic;
+
+        mosi                        :   in  std_logic;
+
+        miso                        :   out std_logic                      := '0'
+
     );
 end CONTROLLER;
 
 architecture Behavioral of CONTROLLER is
 
-type MODE   is (IDLE, REPLY, WAITING);
+    component SPI_topmodule is
+        port(
 
-signal state                :   MODE   := IDLE;
+            clk                     :   in  std_logic;
 
-signal data_controller_i    :   std_logic_vector(15 downto 0);
-signal data_controller_o    :   std_logic_vector(15 downto 0);
-signal w_control_answer     :   std_logic := '0';
-signal b_spi_sleep          :   std_logic := '0';
-signal b_miso               :   std_logic;
-signal b_shift              :   std_logic_vector( 15 downto 0);
+            sck                     :   in  std_logic;
 
-component SPI_topmodule is
-port(
-    clk                     :   in  std_logic;
-    sck                     :   in  std_logic;
-    ss                      :   in  std_logic;
-    mosi                    :   in  std_logic;
-    control_answer          :   in  std_logic;
-    data_controller_i       :   in  std_logic_vector(15 downto 0) := x"0000";
-    miso                    :   out std_logic;
-    data_controller_o       :   out std_logic_vector(15 downto 0) := x"0000";
-    spi_sleep               :   out std_logic := '0'
-    );
-end component;
---------------------------------------------------------------------------------
+            ss                      :   in  std_logic;
+
+            mosi                    :   in  std_logic;
+
+            miso                    :   out std_logic                      := '0';
+
+            control_answer          :   in  std_logic;
+
+            spi_sleep               :   out std_logic                      := '0';
+
+            data_controller_i       :   in  std_logic_vector(15 downto 0);
+
+            data_controller_o       :   out std_logic_vector(15 downto 0)  := (others => '0')
+
+            );
+    end component;
+    -- / / --
+    type MODE is (IDLE, REPLY, WAITING);
+    signal state                    :   MODE   := IDLE;
+
+    signal w_data_too_spitop        :   std_logic_vector(15 downto 0);
+
+    signal w_data_from_spitop       :   std_logic_vector(15 downto 0)     := (others => '0');
+
+    signal w_control_answer         :   std_logic                         := '0';
+
+    signal w_spitop_sleep           :   std_logic                         := '0';
+
+    signal w_shift                  :   std_logic_vector( 15 downto 0)    := (others => '0');
+
+    -- / / --
     begin
---------------------------------------------------------------------------------
 
-    miso <= b_miso;
+    process(clk, w_spitop_sleep) begin
 
-    process(clk, b_spi_sleep)
-    begin
          case( state ) is
+
                 when IDLE =>
-                    if (b_spi_sleep = '1') then
+
+                    if (w_spitop_sleep = '1') then
+
                         w_control_answer <= '0';
-                        b_shift <= data_controller_o;
+
+                        w_shift <= w_data_from_spitop;
+
                         state <= REPLY;
-                    else
-                        state <= IDLE;
-                    end if;
 
+                    else
+
+                        state <= IDLE;
+
+                    end if;
+                    -- / / --
                 when REPLY =>
-                    if( b_shift(3 downto 0) = "0000" ) then
-                        data_controller_i <= x"0000";
+
+                    if( w_shift(3 downto 0) = "0000" ) then
+
+                        w_data_too_spitop <= (others => '0');
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "0001" ) then
-                        data_controller_i <= x"00F0";
+
+                    elsif( w_shift(3 downto 0) = "0001" ) then
+
+                        w_data_too_spitop <= x"00F0";
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "0010" ) then
-                        data_controller_i <= x"0F00";
+
+                    elsif( w_shift(3 downto 0) = "0010" ) then
+
+                        w_data_too_spitop <= x"0F00";
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "0011" ) then
-                        data_controller_i<= x"0FF0";
+
+                    elsif( w_shift(3 downto 0) = "0011" ) then
+
+                        w_data_too_spitop<= x"0FF0";
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "0100" ) then
-                        data_controller_i <= x"F000";
+
+                    elsif( w_shift(3 downto 0) = "0100" ) then
+
+                        w_data_too_spitop <= x"F000";
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "0101" ) then
-                        data_controller_i <= x"F0F0";
+
+                    elsif( w_shift(3 downto 0) = "0101" ) then
+
+                        w_data_too_spitop <= x"F0F0";
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "0110" ) then
-                        data_controller_i <= x"FF00";
+
+                    elsif( w_shift(3 downto 0) = "0110" ) then
+
+                        w_data_too_spitop <= x"FF00";
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "0111" ) then
-                        data_controller_i <= x"FFF0";
+
+                    elsif( w_shift(3 downto 0) = "0111" ) then
+
+                        w_data_too_spitop <= x"FFF0";
+
                         state <= WAITING;
-                    elsif( b_shift(3 downto 0) = "1001" ) then
-                        data_controller_i <= x"AAA0";
+
+                    elsif( w_shift(3 downto 0) = "1001" ) then
+
+                        w_data_too_spitop <= x"AAA0";
+
                         state <= WAITING;
+
                     else
-                        data_controller_i <= x"0FF0";
+
+                        w_data_too_spitop <= x"0FF0";
+
                         state <= WAITING;
+
+                    end if;
+                    -- / / --
+                when WAITING =>
+
+                    if ( w_control_answer = '0' ) then
+
+                        w_control_answer <= '1';
+
+                        state <= WAITING;
+
+                    else
+
+                        state <= IDLE;
+
                     end if;
 
-                when WAITING =>
-                    if ( w_control_answer = '0' ) then                        -- send this data
-                        w_control_answer <= '1';
-                        state <= WAITING;
-                    else
-                        state <= IDLE;
-                    end if;
             end case;
+
     end process;
 --------------------------------------------------------------------------------
     SPI: SPI_TOPMODULE
@@ -124,11 +187,11 @@ end component;
              sck => sck,
              ss => ss,
              mosi => mosi,
-             miso => b_miso,
-             data_controller_i => data_controller_i,
-             data_controller_o => data_controller_o,
+             miso => miso,
+             data_controller_i => w_data_too_spitop,
+             data_controller_o => w_data_from_spitop,
              control_answer => w_control_answer,
-             spi_sleep => b_spi_sleep
+             spi_sleep => w_spitop_sleep
              );
 
 end Behavioral;
